a6e8f446b281cdba9a9d9e2c203669b83f2b3d9a
zhanghuahappy <zhanghua@aragoncs.com>
2017-07-20 18:36:03 +0800

    first commit

---
+++ 0612/myjavascript.js
@@ -0,0 +1,10 @@
+/**
+ * Created by ZhangHua on 2017/6/15.
+ */
+window.onload = function () {
+
+    var my_button = document.getElementById("my_button");
+    my_button.onclick = function () {
+        alert("I am Zhang Hua22");
+    }
+}
